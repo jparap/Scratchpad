9c4123b4ec6347de358742f94f8afe4c5ad62014
Mark Curtis <mark.curtis@datastax.com>
2015-06-05 20:35:32 +0100

    Update createCore.sh

--- assorted_scripts/createCore.sh
+++ assorted_scripts/createCore.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # A very crude example of creating a core file for Solr
+# Edit for the solrconfig.xml and schema.xml files
 
+if [ $# -ne 1 ];then
+	echo "Create solr cre"
+	echo ""
+	echo "Useage: $0 <keyspace>"
+	echo ""
+	exit 1
+fi
 keyspace=$1
 curl http://localhost:8983/solr/resource/$keyspace.user_newsfeed/solrconfig.xml --data-binary @solrconfig-t17362.xml -H 'Content-type:text/xml; charset=utf-8'
 curl http://localhost:8983/solr/resource/$keyspace.user_newsfeed/schema.xml --data-binary @schema-t17362.xml -H 'Content-type:text/xml; charset=utf-8'
