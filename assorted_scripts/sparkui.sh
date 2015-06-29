c488f5739917fe6a86b1d21ef0a6ee44e5307904
Mark Curtis <mark.curtis@datastax.com>
2015-06-29 10:30:51 +0100

    Update sparkui.sh
    Updated for input check, added comments etc

--- assorted_scripts/sparkui.sh
+++ assorted_scripts/sparkui.sh
@@ -2,6 +2,14 @@
 #
 # Grab spark UI from a given IP
 #
+if [ $# -ne 1 ];then
+        echo "Copy the spark UI to local files for browsing"
+        echo ""
+        echo "Useage: $0 <master IP address>"
+        echo ""
+        exit 1
+fi
+
 # 1. Get the first page from the master
 masterIp=$1
 wget "$masterIp:7080" -O master.html
@@ -11,4 +19,4 @@ workerIps=$(grep "worker-" master.html | awk -F "//" '{print $2}' | awk -F ":" '
 echo "Found the following worker IPs: $workerIps"
 
 # 3. Get all the pages recursively
-wget -k -m --adjust-extension --span-hosts "$masterIp:7080" -D ''"$workerIps"''
+wget -q -k -m --adjust-extension --span-hosts "$masterIp:7080" -D ''"$workerIps"''
