da86d44a61e997cad0d091856c5bb82b725e5374
Mark Curtis <mark.curtis@datastax.com>
2015-09-10 09:53:35 +0100

    Update createCore.sh
    Added table variable

--- assorted_scripts/createCore.sh
+++ assorted_scripts/createCore.sh
@@ -5,11 +5,12 @@
 if [ $# -ne 1 ];then
 	echo "Create solr core"
 	echo ""
-	echo "Useage: $0 <keyspace>"
+	echo "Useage: $0 <keyspace> <table>"
 	echo ""
 	exit 1
 fi
 keyspace=$1
-curl http://localhost:8983/solr/resource/$keyspace.user_newsfeed/solrconfig.xml --data-binary @solrconfig-t17362.xml -H 'Content-type:text/xml; charset=utf-8'
-curl http://localhost:8983/solr/resource/$keyspace.user_newsfeed/schema.xml --data-binary @schema-t17362.xml -H 'Content-type:text/xml; charset=utf-8'
-curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=$keyspace.user_newsfeed"
+table=$2
+curl http://localhost:8983/solr/resource/$keyspace.$table/solrconfig.xml --data-binary @solrconfig-test.xml -H 'Content-type:text/xml; charset=utf-8'
+curl http://localhost:8983/solr/resource/$keyspace.$table/schema.xml --data-binary @schema-test.xml -H 'Content-type:text/xml; charset=utf-8'
+curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=$keyspace.$table"
